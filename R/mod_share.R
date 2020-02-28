fd323a55bb081277e1343b70a179ceddcf3ef421
DivadNojnarg <dgranjon@ymail.com>
2020-02-28 22:06:24 +0100

    text edit in share module

--- R/mod_share.R
+++ R/mod_share.R
@@ -1,5 +1,5 @@
 # Module UI
-  
+
 #' @title   mod_share_ui and mod_share_server
 #' @description  A shiny Module.
 #'
@@ -26,13 +26,13 @@ mod_share_ui <- function(id){
     uiOutput(ns("shareFabs"))
   )
 }
-    
+
 # Module Server
-    
+
 #' @rdname mod_share
 #' @export
 #' @keywords internal
-    
+
 mod_share_server <- function(input, output, session, r){
   ns <- session$ns
   
@@ -44,14 +44,15 @@ mod_share_server <- function(input, output, session, r){
     f7Fabs(
       id = ns("shareMenu"),
       extended = TRUE,
-      label = "Open",
+      label = "Share",
       position = "center-bottom",
       sideOpen = "right",
-      a(class="twitter-share-button external",
-        href=glue(value = r$mod_timer$seconds/100, "https://twitter.com/intent/tweet?text=mineSweeper%20score:%20{value}%20"),
-        `data-size`="large",
+      a(class = "twitter-share-button external",
+        href = glue(value = r$mod_timer$seconds/100, "https://twitter.com/intent/tweet?text=mineSweeper%20score:%20{value}%20"),
+        `data-size` = "large",
         onclick = paste0("Shiny.setInputValue(", ns("shareTwitter"), ", true)"),
-        f7Icon("logo_twitter", old = TRUE))
+        f7Icon("logo_twitter", old = TRUE)
+      )
       #morph = TRUE,
       #morphTarget = ".toolbar"
     )
@@ -60,10 +61,10 @@ mod_share_server <- function(input, output, session, r){
   #observe(print(input$shareTwitter))
   #observe(print(input$shareMenu))
 }
-    
+
 ## To be copied in the UI
 # mod_share_ui("share_ui_1")
-    
+
 ## To be copied in the server
 # callModule(mod_share_server, "share_ui_1")
- 
+
