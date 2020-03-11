e5570c9f88d25d5e8befebd31895219c018bfa26
DivadNojnarg <dgranjon@ymail.com>
2020-03-11 21:58:10 +0100

    add links for PWA

--- R/app_ui.R
+++ R/app_ui.R
@@ -8,6 +8,9 @@ app_ui <- function(request) {
     # List the first level UI elements here 
     f7Page(
       title = "deminR",
+      icon = "apple-touch-icon.png",
+      favicon = "favicon.png",
+      manifest = "manifest.json",
       init = f7Init(
         skin = "md",
         theme = "dark",
