6063135d5bb7b2ea180f2ff35326545aa70f4bb2
DivadNojnarg <dgranjon@ymail.com>
2020-04-03 00:01:42 +0200

    minor addition to test mod welcome server

--- tests/testthat/test_mod_welcome.R
+++ tests/testthat/test_mod_welcome.R
@@ -1,17 +1,21 @@
 arg <- reactiveValues(
-  cookies = reactiveValues(),
+  cookies = reactiveValues(user = NULL),
   loginPage = reactiveValues(visible = TRUE)
 )
 
 
+# Test authentication
 tryCatch({
-  testModule(mod_welcome_server,{
+  testModule(mod_welcome_server, {
+    # we start in a non authenticated state
+    expect_equal(authenticated(), FALSE)
     session$setInputs(login_user = "Toto") # set nickname
     session$setInputs(login = 1) # click on submit button
     expect_equal(r$cookies$user, "Toto") # check cookies
+    # once logged, we expect to be authenticated
+    expect_equal(authenticated(), TRUE)
   }, r = arg)
 }, error = function(e){
   print("There was an error!")
   print(e)
 })
-
