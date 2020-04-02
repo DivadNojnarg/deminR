c77c1687aa603edec519965f377f20bde67297c9
DivadNojnarg <dgranjon@ymail.com>
2020-04-03 00:29:13 +0200

    reorder grid utils functions

--- R/utils-grid.R
+++ R/utils-grid.R
@@ -1,18 +1,16 @@
 #' @title Grid generation
 #' @description Several functions to generate the minesweeper grid.
 #'
-#'
 #' @param N size of the grid
 #' @param n_mines number of mines
 #' @param grid the grid
 #' 
 #' @export
 grid_init <- function(N){
-  return(matrix(data = 0, nrow = N, ncol = N))
+  matrix(data = 0, nrow = N, ncol = N)
 }
 
 #' grid_gen_mines
-#' 
 #' 
 #' @rdname grid_init
 grid_gen_mines <- function(grid, n_mines){
@@ -20,12 +18,11 @@ grid_gen_mines <- function(grid, n_mines){
   mines_coord <- sample(1:nrow(grid)^2, size = n_mines)
   # mined cases get the value -999
   grid[mines_coord] <- -999
-  return(grid)
+  grid
 }
 
 #' grid_numbers
 #'
-#' 
 #' @rdname grid_init
 grid_numbers <- function(grid){
   # add padding to the matrix to avoid problems with borders
@@ -50,12 +47,11 @@ grid_numbers <- function(grid){
   # set the number of adjacent mines for each case
   grid[index] <- neigh[index]
   
-  return(grid)
+  grid
 }
 
 #' grid_data
 #'
-#' 
 #' @rdname grid_init
 grid_data <- function(grid, N){
   dt <- data.frame(
@@ -72,7 +68,7 @@ grid_data <- function(grid, N){
   dt$todisplay[dt$value == -999] = emo::ji("bomb")
   dt$todisplay[dt$value == 0] = " "
   
-  return(dt)
+  dt
 }
 
 #' generate_spatial_grid
@@ -114,5 +110,96 @@ generate_spatial_grid <- function(N, n_mines){
     SpP, 
     data=data,
     match.ID = FALSE)
-  return(SpDf)
+  SpDf
+}
+
+
+#' Create a list of cases to reveal
+#' 
+#' This function is used by \link{reveal_onclick}
+#'
+#' @param res Leaflet data.
+#' @param case_id Case to reveal.
+#' @param N Grid size.
+#'
+#' @return List containing cases to reveal.
+#' @export
+#' @rdname reveal
+cases_to_reveal <- function(res, case_id, N) {
+  list_to_test <- c(case_id)
+  list_to_reveal <- c()
+  list_rm_test <- c()
+
+  while (length(list_to_test) > 0) {
+
+    # go through the cases to test
+    for (case_id in list_to_test) {
+
+      # if the cases isn't 0, we add it to the list of cases to delete from the test list
+      if (res$value[res$ID == case_id] != 0) {
+        list_rm_test <- c(list_rm_test, case_id)
+      } else {
+        #### if the case is 0, we add it to the list of cases to reveal
+
+        list_to_reveal <- c(list_to_reveal, case_id)
+
+        #### we add its neighbour to the test list
+
+        i <- as.numeric(unlist(strsplit(case_id, "-"))[2])
+
+        # 4 angles
+        if (i == 1) {
+          case_to_reveal <- c(i + 1, i + N, i + N + 1) # bottom left angle
+        } else if (i == N) {
+          case_to_reveal <- c(i - 1, i + N - 1, i + N) # bottom right angle
+        } else if (i == (N * N - N + 1)) {
+          case_to_reveal <- c(i + 1, i - N + 1, i - N) # top left angle
+        } else if (i == (N * N)) {
+          case_to_reveal <- c(i - 1, i - N, i - N - 1) # top right angle
+        } else if (i %in% (2:N)) {
+          case_to_reveal <- c(i - 1, i + 1, i + N - 1, i + N, i + N + 1) # bottom line
+        } else if (i %in% ((N * N - N + 2):(N * N - 1))) {
+          case_to_reveal <- c(i - 1, i + 1, i - N + 1, i - N, i - N - 1) # top line
+        } else if (i %% N == 0) {
+          case_to_reveal <- c(i - 1, i - N - 1, i + N - 1, i - N, i + N) # right column
+        } else if (i %% N == 1) {
+          case_to_reveal <- c(i + 1, i + N + 1, i - N + 1, i - N, i + N) # left column
+        } else { # inside
+          case_to_reveal <- c(i + 1, i - 1, i + N + 1, i - N - 1, i + N - 1, i - N + 1, i + N, i - N)
+        }
+
+        case_to_reveal <- paste0("case-", case_to_reveal)
+
+        list_to_reveal <- c(list_to_reveal, case_to_reveal)
+        list_to_test <- c(list_to_test, case_to_reveal)
+
+        list_rm_test <- c(list_rm_test, case_id)
+      }
+    }
+
+    list_to_reveal <- unique(list_to_reveal)
+    list_to_test <- unique(list_to_test)
+    list_rm_test <- unique(list_rm_test)
+
+    list_to_test <- list_to_test[!list_to_test %in% list_rm_test]
+  }
+
+  list_to_reveal
+}
+
+
+
+
+#' Reveal grid mines
+#'
+#' @param data Leaflet data.
+#' @param case_id Case to reveal.
+#' @param N Grid size.
+#' @export
+#' @rdname reveal
+reveal_onclick <- function(data, case_id, N){
+  res <- data
+  case_to_reveal <- cases_to_reveal(res, case_id, N)
+  res$hide[res$ID %in% case_to_reveal] <- FALSE
+  res
 }
