ec9d03cdcb56119ad215d6897552d1e356e77f9b
DivadNojnarg <dgranjon@ymail.com>
2020-04-02 22:29:08 +0200

    more tests for scores ui

--- tests/testthat/test-display_scores_ui.R
+++ tests/testthat/test-display_scores_ui.R
@@ -4,4 +4,19 @@ test_that("display scores UI", {
   scoresUI <- mod_display_scores_ui("scores_ui")
   expect_shinytaglist(scoresUI)
   expect_length(scoresUI, 2)
+  
+  # inspect each item individually
+  searchBarUI <- scoresUI[[2]]
+  expect_equal(searchBarUI$attribs$id, "scores_ui-searchbar")
+  # [[1]] is searchbar trigger, [[2]] is the searchbar
+  expect_length(searchBarUI$children, 2)
+  searchBarTriggerUI <- searchBarUI$children[[1]]
+  expect_equal(searchBarTriggerUI$attribs$`data-searchbar`, "#scores_ui-searchScore")
+  
+  # the searchbar tag is composed of a JavaScript part and the HTML part
+  searchBarTag <- searchBarUI$children[[2]]
+  expect_length(searchBarTag, 2)
+  expect_equal(searchBarTag[[1]]$name, "script")
+  # the id attributes must match with the data-searchbar of the searchbarTrigger
+  expect_equal(searchBarTag[[2]]$attribs$id, "scores_ui-searchScore")
 })
