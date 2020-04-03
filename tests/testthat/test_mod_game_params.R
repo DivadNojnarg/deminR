207274d83bbbcd90fd651b6216623f0d7c899957
Gabrielle Devaux <Gabrielle.Devaux@keyruscorp.key>
2020-04-03 16:33:49 +0200

    Completion of test for game parameters module

--- tests/testthat/test_mod_game_params.R
+++ tests/testthat/test_mod_game_params.R
@@ -1,7 +1,3 @@
-library(shiny)
-library(shinyMobile)
-library(testthat)
-library(deminR)
 
 difficulty <- data.frame(
   Level = c("Beginner", "Intermediate", "Advanced"),
@@ -11,10 +7,44 @@ difficulty <- data.frame(
   stringsAsFactors = FALSE
 )
 
-tryCatch({
-  testModule(mod_game_params_server, {
+
+testModule(mod_game_params_server, {
     
-    print(r$mod_grid$data)
+  session$setInputs(level = "Intermediate")
+  # Test data dimension ; if level is intermediate, data should have NxN rows
+  expect_equal(nrow(r$mod_grid$data), difficulty$Size[difficulty$Level=='Intermediate']^2)
+  
+  
+  ### Reset parameters when the user clicks on reload button
+  # Assign some parameters
+  r$click$counter <- 10
+  r$mod_timer$seconds <- 9
+  r$mod_grid$playing <- "won"
+  r$mod_grid$start  <- TRUE 
+  # Simulate click on refresh button
+  session$setInputs(action1_button = 1)
+  # Check the parameters are reset
+  expect_equal(r$click$counter, 0)
+  expect_equal(r$mod_timer$seconds, 0)
+  expect_equal(r$mod_grid$playing , "onload")
+  expect_equal(r$mod_grid$start , FALSE)
+  
+  
+  
+  
+  ### Reset parameters when the user changes the difficulty 
+  # Assign some parameters
+  r$click$counter <- 10
+  r$mod_timer$seconds <- 9
+  r$mod_grid$playing <- "won"
+  r$mod_grid$start  <- TRUE 
+  # Simulate change pf difficulty 
+  session$setInputs(level = "Advanced")
+  # Check the parameters are reset
+  expect_equal(r$click$counter, 0)
+  expect_equal(r$mod_timer$seconds, 0)
+  expect_equal(r$mod_grid$playing , "onload")
+  expect_equal(r$mod_grid$start , FALSE)
     
   }, r = reactiveValues(
     mod_grid = reactiveValues(playing = "onload", start = FALSE, data = generate_spatial_grid(N = 6, n_mines = 5)),
@@ -32,28 +62,8 @@ tryCatch({
       Mines = 5,
       Zoom = 5.8
     )
-  ))
-}, error = function(e){
-  print("There was an error!")
-  print(e)
-})
-# 
-# testModule(mod_game_params_server,{
-#   
-#   print(r$mod_grid$data)
-#   
-# }, r = reactiveValues(
-#   mod_grid = reactiveValues(playing = "onload", start = FALSE, data = generate_spatial_grid(N = 6, n_mines = 5)),
-#   mod_timer = reactiveValues(),
-#   mod_bomb = reactiveValues(),
-#   mod_scores = reactiveValues(refresh = NULL, sendToChat = NULL, autoRefresh = NULL),
-#   click = reactiveValues(counter = 0),
-#   currentTab = reactiveValues(val = NULL),
-#   warrior = reactiveValues(mode = FALSE),
-#   cookies = reactiveValues(),
-#   device = reactiveValues(info = NULL),
-#   settings = difficulty[difficulty$Level == "Beginner",]
-# ))
+))
+
 
 
 
